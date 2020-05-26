b5cec1fffd7c6ca40ff524f058cb254c3930a53a
Bartek Jurczewski <bartek.jurczewski@gmail.com>
2020-05-26 15:30:12 +0200

    First get and foreach

--- src/program/src/main/scala/Main.scala
+++ src/program/src/main/scala/Main.scala
@@ -1,3 +1,36 @@
+import net.ruippeixotog.scalascraper.browser.JsoupBrowser
+import net.ruippeixotog.scalascraper.model._
+import net.ruippeixotog.scalascraper.dsl.DSL._
+import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
+import net.ruippeixotog.scalascraper.dsl.DSL.Parse._
+
 object Main extends App {
-  println("Hello, World!")
+  val browser = JsoupBrowser()
+
+  val query = "monitor"
+  val doc = browser.get("https://allegro.pl/listing?string=" + query)
+
+  var offer = new Offer();
+  offer.desc = doc >> text("._9c44d_1LBF0")
+
+  val items = doc >> elementList("._9c44d_3pyzl article")
+
+  // From each item, extract all the text inside their <a> elements
+  items.map(_ >> allText("a"))
+
+  println(offer.desc)
+
+  items.foreach { println }
+}
+
+class Offer(var id: Int = 0,
+            var desc: String = "",
+            var link: String = "",
+            var title: String = "",
+            var state: String = "",
+            var isSuperSprzedawca: Boolean = false,
+            var price: Double = 0.0,
+            var priceWithShipment: Double = 0.0)
+{
+
 }
