1462be30d41dee572c83a1145ed3da068bc6d9c1
Bartek Jurczewski <bartek.jurczewski@gmail.com>
2020-05-26 16:18:29 +0200

    Parse html to obejcts (desc, price, superSprzedawc

--- src/program/src/main/scala/Main.scala
+++ src/program/src/main/scala/Main.scala
@@ -3,6 +3,7 @@ import net.ruippeixotog.scalascraper.model._
 import net.ruippeixotog.scalascraper.dsl.DSL._
 import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
 import net.ruippeixotog.scalascraper.dsl.DSL.Parse._
+import scala.collection.mutable.ListBuffer
 
 object Main extends App {
   val browser = JsoupBrowser()
@@ -10,17 +11,28 @@ object Main extends App {
   val query = "monitor"
   val doc = browser.get("https://allegro.pl/listing?string=" + query)
 
-  var offer = new Offer();
-  offer.desc = doc >> text("._9c44d_1LBF0")
-
   val items = doc >> elementList("._9c44d_3pyzl article")
+  val offers = new ListBuffer[Offer]()  
 
-  // From each item, extract all the text inside their <a> elements
-  items.map(_ >> allText("a"))
+  items.foreach { el =>
+    var offer = new Offer();
 
-  println(offer.desc)
+    offer.desc = el >> text("._9c44d_LUA1k")
+    offer.price = el >> text("._9c44d_1zemI") //todo: parse to dobule
 
-  items.foreach { println }
+    //SuperSprzedawca
+    val superSprzedawcaText = el >?> text("._9c44d_38vuk")
+    val superSprzedawcaValue: String = superSprzedawcaText match {
+      case None => ""//Or handle the lack of a value another way: throw an error, etc.
+      case Some(s: String) => s //return the string to set your value
+    }
+    offer.isSuperSprzedawca =  if(superSprzedawcaValue.contains("Super Sprzedawcy")) true else false
+
+    offers +=(offer)
+   }
+
+   //Display all offers
+   offers.foreach(println)
 }
 
 class Offer(var id: Int = 0,
@@ -29,8 +41,9 @@ class Offer(var id: Int = 0,
             var title: String = "",
             var state: String = "",
             var isSuperSprzedawca: Boolean = false,
-            var price: Double = 0.0,
+            var price: String = "",
             var priceWithShipment: Double = 0.0)
 {
-
+  override def toString: String =
+      s"Id: $id, Desc: $desc, link: $link, title: $title, state: $state, isSuperSprzedawca: $isSuperSprzedawca, price: $price, priceWithShipment: $priceWithShipment"
 }
