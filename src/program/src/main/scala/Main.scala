b1becf6c84366714e6d29ddd07d50b487ccf8d8c
Bartek Jurczewski <bartek.jurczewski@gmail.com>
2020-06-21 21:07:00 +0200

    Offer extracted

--- src/program/src/main/scala/Main.scala
+++ src/program/src/main/scala/Main.scala
@@ -35,15 +35,3 @@ object Main extends App {
   //Display all offers
   offers.foreach(println)
 }
-
-class Offer(var title: String = "", 
-            var link: String = "",
-            var state: String = "",
-            var isSuperSprzedawca: Boolean = false,
-            var price: Double = 0.0,
-            var priceWithShipment: Double = 0.0)
-{
-
-  override def toString: String =
-      s"Title: $title | state: $state | isSuperSprzedawca: $isSuperSprzedawca | price: $price zl | priceWithShipment: $priceWithShipment zl"
-}
---
+++ src/program/src/main/scala/Offer.scala
@@ -0,0 +1,10 @@
+class Offer(var title: String = "", 
+            var link: String = "",
+            var state: String = "",
+            var isSuperSprzedawca: Boolean = false,
+            var price: Double = 0.0,
+            var priceWithShipment: Double = 0.0)
+{
+  override def toString: String =
+      s"Title: $title | state: $state | isSuperSprzedawca: $isSuperSprzedawca | price: $price zl | priceWithShipment: $priceWithShipment zl"
+}
