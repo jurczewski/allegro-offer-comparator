4f89b3578793fc1ccf8271927259c3e53672c408
P G <pgalewicz@gmail.com>
2020-06-22 21:33:54 +0200

    fixed imports

--- src/program/src/main/scala/Main.scala
+++ src/program/src/main/scala/Main.scala
@@ -11,47 +11,47 @@ import comparator.Offer._
 import filters.MaxPriceFilter
 
 object Main extends App {
-  val browser = JsoupBrowser()
+//  val browser = JsoupBrowser()
+//
+//  val query = "monitor"
+//  val doc = browser.get("https://allegro.pl/listing?string=" + query)
+//
+//  val items = doc >> elementList("._9c44d_3pyzl article")
+//  val offers = new ListBuffer[Offer]()
+//
+//  // Parse html to data
+//  val parser = OfferParser
+//
+//  items.foreach { el =>
+//    var offer = new Offer()
+//
+//    offer.title = parser.titleParser(el)
+//    offer.link = parser.linkParser(el)
+//    offer.state = parser.stateParser(el)
+//    offer.isSuperSprzedawca = parser.superSprzedawcaParser(el)
+//    offer.price = parser.priceParser(el)
+//    offer.priceWithShipment = parser.priceWithShipmentParser(el)
+//
+//    offers +=(offer)
+//  }
+//
+//  //Display all offers
+//  offers.foreach(println)
+//
+//  // Filter
+//  val conf = new Conf(args)
+//  if(conf.bananas.isSupplied) println("apples are: " + conf.apples())
+//  println("bannanas: " + conf.bananas.isSupplied)
 
-  val query = "monitor"
-  val doc = browser.get("https://allegro.pl/listing?string=" + query)
+        var max = new MaxPriceFilter(10)
 
-  val items = doc >> elementList("._9c44d_3pyzl article")
-  val offers = new ListBuffer[Offer]()
+        var filterChain = new OfferFilterChain()
 
-  // Parse html to data
-  val parser = OfferParser
+        var list = List(max.filter)
 
-  items.foreach { el =>
-    var offer = new Offer()
+        var chain = filterChain.createFilterChain(list)
 
-    offer.title = parser.titleParser(el)
-    offer.link = parser.linkParser(el)
-    offer.state = parser.stateParser(el)
-    offer.isSuperSprzedawca = parser.superSprzedawcaParser(el)
-    offer.price = parser.priceParser(el)
-    offer.priceWithShipment = parser.priceWithShipmentParser(el)
-
-    offers +=(offer)
-  }
-
-  //Display all offers
-  offers.foreach(println)
-
-  // Filter
-  val conf = new Conf(args)
-  if(conf.bananas.isSupplied) println("apples are: " + conf.apples())
-  println("bannanas: " + conf.bananas.isSupplied)
-
-    //    var max = new MaxPriceFilter(10)
-    //
-    //    var filterChain = new OfferFilterChain()
-    //
-    //    var list = List(max.filter)
-    //
-    //    var chain = filterChain.createFilterChain(list)
-    //
-    //    var lista = List(new Offer("pasi", price = 5), new Offer("nie pasi", price = 30))
-    //        .filter(chain)
-    //        .foreach(o => println(o))
+        var lista = List(new Offer("pasi", price = 5), new Offer("nie pasi", price = 30))
+            .filter(chain)
+            .foreach(o => println(o))
 }
--- src/program/src/main/scala/filters/MaxPriceFilter.scala
+++ src/program/src/main/scala/filters/MaxPriceFilter.scala
@@ -1,7 +1,7 @@
 package filters
 
-import comparator.{Filter, Offer}
+import comparator.Offer
 
-class MaxPriceFilter(val maxPrice: Int) extends Filter {
+class MaxPriceFilter(val maxPrice: Double) extends Filter {
     override var filter: Offer => Boolean = o => o.price <= maxPrice
 }
