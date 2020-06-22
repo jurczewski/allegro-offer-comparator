2e4b5cda14fa046afa2e436ca6dfd0668a0ec369
Bartek Jurczewski <bartek.jurczewski@gmail.com>
2020-06-22 21:37:50 +0200

    comments fix

--- src/program/src/main/scala/Main.scala
+++ src/program/src/main/scala/Main.scala
@@ -11,47 +11,48 @@ import comparator.Offer._
 import filters.MaxPriceFilter
 
 object Main extends App {
-//  val browser = JsoupBrowser()
-//
-//  val query = "monitor"
-//  val doc = browser.get("https://allegro.pl/listing?string=" + query)
-//
-//  val items = doc >> elementList("._9c44d_3pyzl article")
-//  val offers = new ListBuffer[Offer]()
-//
-//  // Parse html to data
-//  val parser = OfferParser
-//
-//  items.foreach { el =>
-//    var offer = new Offer()
-//
-//    offer.title = parser.titleParser(el)
-//    offer.link = parser.linkParser(el)
-//    offer.state = parser.stateParser(el)
-//    offer.isSuperSprzedawca = parser.superSprzedawcaParser(el)
-//    offer.price = parser.priceParser(el)
-//    offer.priceWithShipment = parser.priceWithShipmentParser(el)
-//
-//    offers +=(offer)
-//  }
-//
-//  //Display all offers
-//  offers.foreach(println)
-//
-//  // Filter
-//  val conf = new Conf(args)
-//  if(conf.bananas.isSupplied) println("apples are: " + conf.apples())
-//  println("bannanas: " + conf.bananas.isSupplied)
+  val browser = JsoupBrowser()
 
-        var max = new MaxPriceFilter(10)
+  val query = "monitor"
+  val doc = browser.get("https://allegro.pl/listing?string=" + query)
 
-        var filterChain = new OfferFilterChain()
+  val items = doc >> elementList("._9c44d_3pyzl article")
+  val offers = new ListBuffer[Offer]()
 
-        var list = List(max.filter)
+  // Parse html to data
+  val parser = OfferParser
 
-        var chain = filterChain.createFilterChain(list)
+  items.foreach { el =>
+    var offer = new Offer()
 
-        var lista = List(new Offer("pasi", price = 5), new Offer("nie pasi", price = 30))
-            .filter(chain)
-            .foreach(o => println(o))
+    offer.title = parser.titleParser(el)
+    offer.link = parser.linkParser(el)
+    offer.state = parser.stateParser(el)
+    offer.isSuperSprzedawca = parser.superSprzedawcaParser(el)
+    offer.price = parser.priceParser(el)
+    offer.priceWithShipment = parser.priceWithShipmentParser(el)
+
+    offers +=(offer)
+  }
+
+  //Display all offers
+  offers.foreach(println)
+
+  // Filter
+  val conf = new Conf(args)
+  if(conf.bananas.isSupplied) println("apples are: " + conf.apples())
+  println("bannanas: " + conf.bananas.isSupplied)
+
+  // var max = new MaxPriceFilter(10)
+
+  // var filterChain = new OfferFilterChain()
+
+  // var list = List(max.filter)
+
+  // var chain = filterChain.createFilterChain(list)
+
+  // var lista = List(new Offer("pasi", price = 5),
+  //                  new Offer("nie pasi", price = 30))
+  //     .filter(chain)
+  //     .foreach(o => println(o))
 }
