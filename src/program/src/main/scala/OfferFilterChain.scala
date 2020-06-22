628fc28994840d3ef70d653b0815e7ed45b48961
Bartek Jurczewski <bartek.jurczewski@gmail.com>
2020-06-22 22:40:12 +0200

    offer filter cleanup

--- src/program/src/main/scala/OfferFilterChain.scala
+++ src/program/src/main/scala/OfferFilterChain.scala
@@ -9,12 +9,4 @@ class OfferFilterChain{
     private def mergeFilter(f1: Offer => Boolean, f2: Offer => Boolean): Offer => Boolean = {
         a => f1(a) && f2(a)
     }
-//
-//    var wieksze5: Int => Boolean = el => el > 5
-//    var podzielne3: Int => Boolean = el => el % 3 == 0
-//    var mniejsze20: Int => Boolean = el => el < 20
-//
-//    val filters = List(wieksze5, podzielne3, mniejsze20)
-//
-//    val filterChain = createFilterChain(filters)
 }
